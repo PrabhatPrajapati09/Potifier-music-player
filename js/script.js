1f27fb02ee67acc766e75893c746ba8c76cc3223
PrabhatPrajapati09 <prabhatprajapati0099@gmail.com>
2025-04-30 13:28:39 +0530

    songs

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ let currFolder;
 
 async function getSongs(folder) {
     currFolder=folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+    let a = await fetch(`https://github.com/PrabhatPrajapati09/Potifier-music-player/tree/main/songs/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -73,7 +73,7 @@ const playMusic = (track, pause=false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs`);
+    let a = await fetch(`https://github.com/PrabhatPrajapati09/Potifier-music-player/tree/main/songs`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -86,7 +86,7 @@ async function displayAlbums() {
         if(e.href.includes("/songs/")){
             let folder = (e.href.split("/").slice(-1)[0]);
             //get the metadata of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+            let a = await fetch(`https://github.com/PrabhatPrajapati09/Potifier-music-player/tree/main/songs/${folder}/info.json`);
             let response = await a.json();
             console.log(response);
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
